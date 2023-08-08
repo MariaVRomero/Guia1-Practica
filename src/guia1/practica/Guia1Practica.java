d06f7bf095556ed250225e256453119eb7c63303
MariaVRomero <141704203+MariaVRomero@users.noreply.github.com>
2023-08-08 13:06:17 -0300

    Initial commit

---
+++ src/guia1/practica/Guia1Practica.java
@@ -0,0 +1,14 @@
+
+package guia1.practica;
+
+
+public class Guia1Practica {
+
+   
+    public static void main(String[] args) {
+        
+        System.out.println("Hola!");    
+    
+    }
+    
+}
