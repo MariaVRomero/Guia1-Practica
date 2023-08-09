b0c711b44c14272b53c6dda32ff0619fb7943300
dard23 <141675448+dard23@users.noreply.github.com>
2023-08-09 15:24:16 -0300

    Update darioruizdiaz.java

--- src/guia1/practica/darioruizdiaz.java
+++ src/guia1/practica/darioruizdiaz.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package guia1.practica;
 
-/**
- *
- * @author dard
- */
+
 public class darioruizdiaz {
     
 }
