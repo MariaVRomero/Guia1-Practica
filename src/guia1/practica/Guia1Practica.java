48fb97468f6ba4db427b26e28671bc88a0548e33
MariaVRomero <141704203+MariaVRomero@users.noreply.github.com>
2023-08-10 10:30:04 -0300

    Agregue un sout

--- src/guia1/practica/Guia1Practica.java
+++ src/guia1/practica/Guia1Practica.java
@@ -7,7 +7,7 @@ public class Guia1Practica {
    
     public static void main(String[] args) {
         
-        System.out.println("Hola!");  
+        System.out.println("Hola, Soy Maria Victoria");  
         System.out.println("Hola, soy francisco me sumo al proyecto☻");
         System.out.println("Hola, soy lucas");
     
